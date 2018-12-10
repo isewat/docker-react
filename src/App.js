ab2ef7808edda24c29a57f40a8214eda9a02e001
Ralf <isewat@gmail.com>
2018-12-10 17:03:18 +0100

    message to thomas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>I was changed</p>
+          <p>Hey Thomas, jetzt steht der Weltherrschaft nichts mehr im Wege!</p>
           <a
             className="App-link"
             href="https://reactjs.org"
